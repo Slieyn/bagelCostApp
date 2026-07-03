212378d98c79ce63ba6877ae0c21c574da5d8663
Slieyn <163781558+Slieyn@users.noreply.github.com>
2026-07-03 20:27:40 +0200

    Add project files.

---
+++ bagelCostApp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class BagelCostApp
+{
+    static void Main()
+    {
+        Console.Write("Enter the number of bagels ordered: ");
+
+        int numBagels;
+        while (!int.TryParse(Console.ReadLine(), out numBagels) || numBagels < 0)
+        {
+            Console.Write("Invalid input. Please enter a valid whole number of bagels: ");
+        }
+
+        int pricePerBagel;
+
+        if (numBagels < 6)
+        {
+            pricePerBagel = 75;
+        }
+        else
+        {
+            pricePerBagel = 60;
+        }
+
+        int totalCost = numBagels * pricePerBagel;
+
+        Console.WriteLine($"Number of bagels: {numBagels}");
+        Console.WriteLine($"Price per bagel: {pricePerBagel} cents");
+        Console.WriteLine($"Total cost: {totalCost} cents (${totalCost / 100.0:F2})");
+    }
+}
